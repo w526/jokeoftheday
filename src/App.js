e7a6b259d2af40109421435d907bff6943018e46
Ville Similä <ville-simila@hotmail.com>
2022-10-11 15:00:23 +0300

    valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const URL = 'https://api.jokes.one/jod'
 
 function App() {
+  const [title, setTitle] = useState('')
+  const [text, setText] = useState('')
+
+  useEffect(() => {
+    axios.get(URL).then((response) => {
+      console.log(response)
+      const joke = response.data.contents.jokes[0].joke
+      setTitle(joke.title)
+      setText(joke.text)
+    }).catch(error => {
+      console.log(error)
+      alert("Retrieving the joke failed")
+    })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h3>
+        Joke of the day
+      </h3>
+      <h4>{title}</h4>
+      <p>{text}</p>
     </div>
   );
 }
